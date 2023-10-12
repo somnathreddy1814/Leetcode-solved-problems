da21a7f049a9c8f7565e4de24f14a84f956e5ede
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2023-10-13 01:45:36 +0530

    Time: 642 ms (63.59%), Space: 33.5 MB (37.83%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,28 +1,27 @@
 class Solution:
-    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        sm=0
-        mxsm=0
+    def maximumSubarraySum(self, arr: List[int], k: int) -> int:
         i=0
         j=0
-        seen=defaultdict(int) #using dictonary as removing an element T.C is O(1)
-        while(j<len(nums)):
-            if nums[j] not in seen:
-                sm+=nums[j]
-                seen[nums[j]]=1
+        sm=0
+        mxsm=0
+        seen=defaultdict(int)
+        while(j<len(arr)):
+            if arr[j] not in seen:
+                sm+=arr[j]
+                seen[arr[j]]=1
                 if j-i+1<k:
                     j+=1
                 elif j-i+1==k:
                     mxsm=max(mxsm,sm)
-                    # print(mxsm)
-                    sm-=nums[i]
-                    del seen[nums[i]]
+                    sm-=arr[i]
+                    del seen[arr[i]]
                     i+=1
                     j+=1
-                    
             else:
-                sm-=nums[i]
-                del seen[nums[i]]
+                sm-=arr[i]
+                del seen[arr[i]]
                 i+=1
-                    
-                
         return mxsm
+                
+            
+        
