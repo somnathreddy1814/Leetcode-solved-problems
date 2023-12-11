b8eb61b370f99e41868b3cee93203f473a257ae9
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2023-12-11 16:45:48 +0530

    Time: 96 ms (40.00%), Space: 18.1 MB (19.18%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.py
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.py
@@ -1,19 +1,11 @@
 class Solution(object):
     def findSpecialInteger(self, arr):
-        size = len(arr)
-        qtr = size // 4
-        cnt = 1
-        p = arr[0]
+        cnts=defaultdict(int)
+        for num in arr:
+            cnts[num]+=1
+        target=len(arr)//4
+        for key,value in cnts.items():
+            if value>target:
+                return key
+        return -1
         
-        for i in range(1, size):
-            if p == arr[i]:
-                cnt += 1
-            else:
-                cnt = 1
-            
-            if cnt > qtr:
-                return arr[i]
-            
-            p = arr[i]
-
-        return p
