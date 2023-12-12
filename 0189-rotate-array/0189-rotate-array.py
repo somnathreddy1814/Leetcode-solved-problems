4b760712aa8fee32eb11260e2ab320edff4b42e8
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2023-12-12 12:50:26 +0530

    Time: 169 ms (95.34%), Space: 27.7 MB (55.41%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,17 +1,10 @@
 class Solution:
-    def rotate(self, arr: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        k=k%len(arr)
-        def rev(arr,start,end):
-            while(start<end):
-                arr[start],arr[end]=arr[end],arr[start]
-                start+=1
-                end-=1
-        
-        rev(arr,0,len(arr)-1)
-        rev(arr,0,k-1)
-        rev(arr,k,len(arr)-1)
-        
+    def rotate(self, nums: list[int], k: int) -> None:
+        if k> len(nums):
+            k=k%len(nums)
+            
+        my_dic={}
+        my_dic['first']=(nums[-k:])
+        my_dic['second']=nums[:-k]
+        nums[:]=my_dic['first'] + my_dic['second']
         
