ce4f3bc365589db99a8f5551a6d9482182967f60
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-01-04 04:22:01 +0530

    Time: 86 ms (92.45%), Space: 19.3 MB (19.52%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -4,9 +4,7 @@ class Solution:
         final_count=0
         for i in range(len(bank)):
             current_count=0
-            for j in range(len(bank[i])):
-                if bank[i][j]=='1':
-                    current_count+=1
+            current_count=bank[i].count('1')
             final_count+=previous_count*current_count
             if current_count==0:
                 continue
