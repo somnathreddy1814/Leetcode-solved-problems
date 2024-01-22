b5992c8a6c62e9cfb23023bd996787e1d37e4fd7
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-01-22 09:15:16 +0530

    Time: 143 ms (98.45%), Space: 18.8 MB (37.76%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findErrorNums(self, nums: List[int]) -> List[int]:
-        return [sum(nums) - sum(set(nums)), sum(range(1, len(nums)+1)) - sum(set(nums))]
-        
-        
+    def findErrorNums(self, nums: list[int]) -> list[int]:
+        n,a,b=len(nums),sum(nums),sum(set(nums))
+        s=sum([i for i in range(1,n+1)])
+        return [a-b,s-b]
