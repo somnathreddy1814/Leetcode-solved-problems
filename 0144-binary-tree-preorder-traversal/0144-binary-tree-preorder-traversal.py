c616cd0ab6825e802db2d1ef2ad9538857e45963
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-01-28 11:28:11 +0530

    Time: 42 ms (28.21%), Space: 16.4 MB (78.19%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -1,14 +1,14 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if root is None:
-            return []
-        return [root.val]+      self.preorderTraversal(root.left)+self.preorderTraversal(root.right)
-
-  
+        ans = []
+        stack = [root]
+        
+        while stack:
+            temp = stack.pop()
+            
+            if temp:
+                ans.append(temp.val)
+                stack.append(temp.right) #as we are using stack which works on LIFO, we need to push right tree first so that left will be popped out
+                stack.append(temp.left)
+                
+        return ans
