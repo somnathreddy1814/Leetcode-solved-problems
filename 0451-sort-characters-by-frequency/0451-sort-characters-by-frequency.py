81a00953fa450336e1ee0c28d4df25d89e6c1f7a
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-02-07 06:48:39 +0530

    Time: 50 ms (49.61%), Space: 19 MB (22.81%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -3,6 +3,37 @@ class Solution:
         count=Counter(s)
        
         ans=[]
-        for key,val in count.most_common():
+        for key,val in sorted(count.items(),key=lambda x:(-x[1],x[0])):
             ans+=[key]*val
         return "".join(ans)
+    
+    
+    
+    
+# def frequencySort(self, s: str) -> str:
+# 	# Count the occurence on each character
+# 	cnt = collections.defaultdict(int)
+# 	for c in s:
+# 		cnt[c] += 1
+	
+# 	# Sort and Build string
+# 	res = []
+# 	for k, v in sorted(cnt.items(), key = lambda x: -x[1]):
+# 		res += [k] * v
+# 	return "".join(res)
+
+
+# class Solution:
+#     def frequencySort(self, s: str) -> str:     #   Example:   s = "bAaCbb"
+
+#         ctr = Counter(s)                        #       ctr = {'b':3, 'A':1, 'a':1, 'C':1}
+
+#         d   = list(ctr.items())                 #         d = [('b',3), ('C',1), ('a',1), ('A',1)]
+
+#         d.sort(key = lambda x: (-x[1], x[0]))   #         d = [('b',3), ('A',1), ('C',1), ('a',1)]
+
+#         words = [ch*n for ch, n in d]           #     words = ['bbb', 'A', 'C', 'a']
+
+#         ans = ''.join(words)                    #       ans = 'bbbACa'
+
+#         return ans
