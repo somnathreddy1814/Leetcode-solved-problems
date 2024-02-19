c838022475ede241baa44fc994f0871564cadc72
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-02-19 19:45:01 +0530

    Time: 34 ms (91.48%), Space: 17.3 MB (76.50%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,38 +1,19 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-from collections import deque
-from typing import List, Optional
-
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        ans = []
-        if root is None:
-            return ans        
-        q = deque()
-        q.append(root)        
-        while q:
-            level_vals = []  # Store the values at the current level
-            level_size = len(q)
-            for _ in range(level_size):
-                temp = q.popleft()
-                level_vals.append(temp.val)
-                
-                if temp.left:
-                    q.append(temp.left)
-                if temp.right:
-                    q.append(temp.right)
+        result = []
+        count = 0
+
+        def level(count , root):
+            if root == None :
+                return 
+
+            if len(result) <= count:
+                result.append([])    
             
-            ans.append(level_vals)  # Add the values at the current level to the result list
-        
-        return ans
+            result[count].append(root.val)
+            count += 1
+            level(count , root.left)
+            level(count , root.right) 
+
+        level(count , root)
+        return result
