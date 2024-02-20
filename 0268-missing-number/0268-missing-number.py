86182d8bfd81f66dadb0180506f3ab1c3728948c
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-02-20 06:54:54 +0530

    Time: 101 ms (95.70%), Space: 17.8 MB (64.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,12 @@
 class Solution:
     def missingNumber(self, nums1: List[int]) -> int:    
-        nums2=[i for i in range(len(nums1))]
-        if max(nums1)==max(nums2):
+        sm=0
+        for i in range(len(nums1)):
+            sm+=i
+        if max(nums1)==len(nums1)-1:
             return max(nums1)+1
         else:
-            return sum(nums2)-(sum(nums1)-max(nums1))
+            return sm-(sum(nums1)-max(nums1))
         
         
         
