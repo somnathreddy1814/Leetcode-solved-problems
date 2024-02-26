25b7a337a23e998540337196b665841b82619f1a
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-02-26 08:14:43 +0530

    Time: 33 ms (75.87%), Space: 16.7 MB (45.81%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -8,18 +8,18 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         res1=[]
         res2=[]
-        def inorder(node,tree):
+        def preorder(node,tree):
             if node is None:
                 tree.append("null")
                 return 0
             tree.append(node.val)
-            inorder(node.left,tree)
-            inorder(node.right,tree)
+            preorder(node.left,tree)
+            preorder(node.right,tree)
             
             
             return tree
-        res1=inorder(p,res1)
-        res2=inorder(q,res2)
+        res1=preorder(p,res1)
+        res2=preorder(q,res2)
         print(res1)
         print(res2)
         return res1==res2 
