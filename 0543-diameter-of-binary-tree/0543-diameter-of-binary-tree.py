e8b90da75a4841dd60bd975e1bb225367ce89fce
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-02-27 06:43:20 +0530

    Time: 42 ms (62.04%), Space: 17.7 MB (74.69%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -7,16 +7,13 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         ans=0
-        def diameter(node):
+        def fun(node):
             nonlocal ans
             if node is None:
                 return 0
-            left=diameter(node.left)
-            right=diameter(node.right)
+            left=fun(node.left)
+            right=fun(node.right)
             ans=max(ans,left+right)
             return 1+max(left,right)
-        diameter(root)
+        fun(root)
         return ans
-            
-            
-        
