b42718a04276dbf4d834b1d2416f914439ebf0d8
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-02-27 07:07:10 +0530

    Time: 46 ms (99.64%), Space: 20.9 MB (67.30%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -13,30 +13,8 @@ class Solution:
                 return 0
             left=max(fun(node.left),0)
             right=max(fun(node.right),0)
-            mx_path=node.val+left+right
-            mx=max(mx,mx_path)
+            path=node.val+left+right
+            mx=max(mx,path)
             return node.val+max(left,right)
         fun(root)
         return mx
-            
-            
-# class Solution:
-#     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-#         self.max_path = float("-inf")
-#         self.helper(root)
-        
-#         return self.max_path
-        
-#     def helper(self, root):
-#         if not root:
-#             return 0
-        
-#         left_res = max(self.helper(root.left), 0)
-#         right_res = max(self.helper(root.right), 0)
-        
-#         # can use both child for current root
-#         max_path_for_this_root = root.val + left_res + right_res
-#         self.max_path = max(self.max_path, max_path_for_this_root)
-        
-#         # to be added to parent path, can only pick one of the child
-#         return root.val + max(left_res, right_res)  
