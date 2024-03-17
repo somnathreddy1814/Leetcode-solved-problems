82ceb703ce9473f768099558f7f6ee23e50991bb
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-03-17 14:52:56 +0530

    Time: 365 ms (14.01%), Space: 16.4 MB (84.76%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,14 +4,17 @@ class Solution:
             return 0
         if n==1:
             return 1
-        n1=0
-        n2=1
-        n3=0
-        for i in range(1,n):
-            n3=n1+n2
-            n1=n2
-            n2=n3
-        return n3
+        return self.fib(n-1)+self.fib(n-2)
+    
+    
+        # n1=0
+        # n2=1
+        # n3=0
+        # for i in range(1,n):
+        #     n3=n1+n2
+        #     n1=n2
+        #     n2=n3
+        # return n3
             
             
             
