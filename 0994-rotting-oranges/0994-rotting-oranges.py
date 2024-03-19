3bba5e2b0f97dfb999dc0898a720f2070cb92ac3
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-03-19 11:26:49 +0530

    Time: 55 ms (30.14%), Space: 16.6 MB (67.54%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,26 +1,26 @@
 class Solution:
-    def bfs(self,grid, queue, count_fresh):
-        rows, cols = len(grid), len(grid[0])
+    def bfs(self,grid, q, count_fresh):
+        n, m = len(grid), len(grid[0])
         countMin = 0
         cnt = 0
-        dx = [0, 0, 1, -1]
-        dy = [1, -1, 0, 0]
+        delrow = [1,-1,0,0]
+        delcol = [0,0,-1,1]
 
-        while queue:
-            size = len(queue)
+        while q:
+            size = len(q)
             cnt += size
             for _ in range(size):
-                point = queue.popleft()
-                for j in range(4):
-                    x = point[0] + dx[j]
-                    y = point[1] + dy[j]
+                row,col = q.popleft()
+                for i in range(4):
+                    nrow = row + delrow[i]
+                    ncol = col + delcol[i]
 
-                    if x < 0 or y < 0 or x >= rows or y >= cols or grid[x][y] == 0 or grid[x][y] == 2:
+                    if nrow < 0 or ncol < 0 or nrow >= n or ncol >= m or grid[nrow][ncol] == 0 or grid[nrow][ncol] == 2:
                         continue
 
-                    grid[x][y] = 2
-                    queue.append((x, y))
-            if queue:
+                    grid[nrow][ncol] = 2
+                    q.append((nrow, ncol))
+            if q:
                 countMin += 1
 
         return countMin if count_fresh == cnt else -1
@@ -28,18 +28,18 @@ class Solution:
 
         if grid is None or len(grid) == 0:
             return 0
-        rows, cols = len(grid), len(grid[0])
-        queue = deque()
+        n, m = len(grid), len(grid[0])
+        q = deque()
         count_fresh = 0
 
-        for i in range(rows):
-            for j in range(cols):
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j] == 2:
-                    queue.append((i, j))
+                    q.append((i, j))
                 if grid[i][j] != 0:
                     count_fresh += 1
 
         if count_fresh == 0:
             return 0
 
-        return self.bfs(grid, queue, count_fresh)
+        return self.bfs(grid, q, count_fresh)
