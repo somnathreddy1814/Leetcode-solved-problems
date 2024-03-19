5561c971ff84dc6db23bdd64c5ea554cff4675cc
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-03-19 10:28:15 +0530

    Time: 257 ms (44.54%), Space: 18.8 MB (83.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,22 +2,24 @@ from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def bfs(row,col,vis,grid):
-            vis[row][col]=1
             q=deque()
-            q.append((row,col))
             n=len(grid)
             m=len(grid[0])
+            vis[row][col]=1
+            q.append((row,col))
             while(q):
                 row,col=q.popleft()
                 delrow=[-1,0,1,0]
-                delcol=[0,-1,0,1]
+                delcol=[0,-1,0,+1]
                 for i in range(4):
                     nrow=delrow[i]+row
                     ncol=delcol[i]+col
-                    if 0<=nrow<n and 0<=ncol<m and grid[nrow][ncol]=='1' and vis[nrow][ncol]==0:
+                    if 0<=nrow<n and 0<=ncol<m and vis[nrow][ncol]==0 and grid[nrow][ncol]=='1':
                         vis[nrow][ncol]=1
                         q.append((nrow,ncol))
-            
+                        
+        
+        
         n=len(grid)
         m=len(grid[0])
         vis=[[0]*m for _ in range(n)]
@@ -28,5 +30,4 @@ class Solution:
                     cnt+=1
                     bfs(i,j,vis,grid)
         return cnt
-            
-          
+        
