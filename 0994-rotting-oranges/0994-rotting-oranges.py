e62c6304e7e21f02fd2416026ed901167e810ce7
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-03-25 10:42:41 +0530

    Time: 50 ms (60.54%), Space: 16.6 MB (66.83%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,53 +1,47 @@
 from collections import deque
+
 class Solution:
 
-    #Function to find minimum time required to rot all oranges. 
-	def orangesRotting(self, grid):
-		#Code here
-		def bfs(grid,q,cnt_fresh):
-		    n=len(grid)
-		    m=len(grid[0])
-		    cnt=0  #to check if all oranges marked fresh are touched or not
-		    min=0  #count of minutes to return 
-		    delrow=[1,-1,0,0]#as we can move in 4 directons these are possible changes
-		    delcol=[0,0,1,-1]
-		    while(q):
-		        for _ in range(len(q)):
-		            row,col=q[0][0]
-		            t=q[0][1]
-		            for i in range(4):
-		                nrow=row+delrow[i]
-		                ncol=col+delcol[i]
-		                if 0<=nrow<n and 0<=ncol<m and grid[nrow][ncol]==1:
-		                    cnt+=1
-		                    grid[nrow][ncol]=2 #modify neigbours
-		                  
-		                    q.append(((nrow,ncol),t+1)) #append locations to queue to explore those as well
-		        q.popleft()
-		                
-		        if q:
-		            min+=1 #update levels
-		    if cnt!=cnt_fresh:
-		        return -1
-		    return t
-		    
-		n=len(grid)
-		m=len(grid[0])
-		q=deque()
-		cnt_fresh=0
-		t=0
-		if n==0 or grid is None:
-		    return 0
-		for i in range(n):
-		    for j in range(m):
-		        if grid[i][j]==2:
-		            q.append(((i,j),t))
-		        if grid[i][j]==1:
-		            cnt_fresh+=1
-
-		if cnt_fresh==0:
-		    return 0
-		return bfs(grid,q,cnt_fresh)
-		 
-
-
+    # Function to find minimum time required to rot all oranges. 
+    def bfs(self, grid, q, cnt_fresh):
+        n = len(grid)
+        m = len(grid[0])
+        cnt = 0
+        min_time = 0
+        del_col = [0, 0, -1, 1]
+        del_row = [1, -1, 0, 0]
+        while q:
+            size = len(q)
+            
+            for _ in range(size):
+                row, col = q.popleft()
+                for i in range(4):
+                    nrow = row + del_row[i]
+                    ncol = col + del_col[i]
+                    if 0 <= nrow < n and 0 <= ncol < m and grid[nrow][ncol] == 1:
+                        grid[nrow][ncol] = 2
+                        cnt+=1
+                        q.append((nrow, ncol))
+            if q:
+                min_time += 1
+        if cnt != cnt_fresh:
+            return -1
+        return min_time
+    
+    def orangesRotting(self, grid):
+        # Code here
+        n = len(grid)
+        m = len(grid[0])
+        cnt_fresh = 0
+        q = deque()
+        if n == 0 or grid is None:
+            return 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    cnt_fresh += 1
+                if grid[i][j] == 2:
+                    q.append((i, j))
+        if cnt_fresh == 0:
+            return 0
+        return self.bfs(grid, q, cnt_fresh)
