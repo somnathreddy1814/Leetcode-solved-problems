e193368d4a8a00d3c279f314c2d3e0345949bbe3
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-03-25 12:18:54 +0530

    Time: 463 ms (55.39%), Space: 20.5 MB (23.30%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,33 +1,25 @@
-# class Solution:
-#     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
 class Solution:
-    def updateMatrix(self, grid):
-        n = len(grid)
-        m = len(grid[0])
-        vis = [[0] * m for _ in range(n)]
-        dist = [[0] * m for _ in range(n)]
-        q = deque()
-        
+    def updateMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        n=len(grid)
+        m=len(grid[0])
+        q=deque()
+        vis=[[0]*m for _ in range(n)]
+        dist=[[0]*m for _ in range(n)]
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == 0:
-                    q.append(((i, j), 0))
-                    vis[i][j] = 1
-                else:
-                    vis[i][j] = 0
-        
-        delrow = [-1, 0, +1, 0]
-        delcol = [0, +1, 0, -1]
-        
-        while q:
-            (row, col), steps = q.popleft()
-            dist[row][col] = steps
-            
+                if grid[i][j]==0:
+                    vis[i][j]=1
+                    q.append(((i,j),0))
+        delrow=[0,0,1,-1]
+        delcol=[1,-1,0,0]
+        while(q):
+            (row,col),steps=q.popleft()
+            dist[row][col]=steps
             for i in range(4):
-                nrow = row + delrow[i]
-                ncol = col + delcol[i]
-                if 0 <= nrow < n and 0 <= ncol < m and vis[nrow][ncol] == 0:
-                    vis[nrow][ncol] = 1
-                    q.append(((nrow, ncol), steps + 1))
-        
+                nrow=row+delrow[i]
+                ncol=col+delcol[i]
+                if 0<=nrow<n and 0<=ncol<m and grid[nrow][ncol]==1 and vis[nrow][ncol]==0:
+                    vis[nrow][ncol]=1
+                    q.append(((nrow,ncol),steps+1))
         return dist
+        
