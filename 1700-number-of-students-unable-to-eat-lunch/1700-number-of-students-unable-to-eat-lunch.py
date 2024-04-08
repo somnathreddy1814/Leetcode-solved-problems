50ce985e6ed92999c25e1d55638c2ae2504bbd3b
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-04-08 22:25:46 +0530

    Time: 38 ms (72.82%), Space: 16.7 MB (16.84%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countStudents(self, A, B):
+        count = collections.Counter(A)
+        n, k = len(A), 0
+        while k < n and count[B[k]]:
+            count[B[k]] -= 1
+            k += 1
+        return n - k
+        
+                
+        
