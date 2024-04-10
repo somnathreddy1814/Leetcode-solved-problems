0675ddcc435a416c6d7dba942c6596be0b20f1cf
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-04-10 06:27:33 +0530

    Time: 42 ms (80.14%), Space: 16.9 MB (9.42%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
@@ -0,0 +1,11 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        deck.sort(reverse=True)
+        queue = deque([deck[0]])
+        
+        for i in range(1, len(deck)):
+            queue.appendleft(queue.pop())
+            queue.appendleft(deck[i])
+            # print(queue)
+        
+        return queue
