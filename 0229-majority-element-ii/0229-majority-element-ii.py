32c41ab896c2405ec22d04f6ef8f40aeb06ef540
somnathreddy1814 <102971222+somnathreddy1814@users.noreply.github.com>
2024-04-27 13:37:49 +0530

    Time: 102 ms (39.36%), Space: 18.1 MB (26.41%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,53 +1,35 @@
 class Solution:
-    def majorityElement(self, v: List[int]) -> List[int]:
-
-        n = len(v) # size of the array
-
-        cnt1, cnt2 = 0, 0 # counts
-        el1, el2 = float('-inf'), float('-inf') # element 1 and element 2
-
-        # applying the Extended Boyer Moore’s Voting Algorithm:
-        for i in range(n):
-            if cnt1 == 0 and el2 != v[i]:
-                cnt1 = 1
-                el1 = v[i]
-            elif cnt2 == 0 and el1 != v[i]:
-                cnt2 = 1
-                el2 = v[i]
-            elif v[i] == el1:
-                cnt1 += 1
-            elif v[i] == el2:
-                cnt2 += 1
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        cnt1=0
+        cnt2=0
+        ele1=float('inf')
+        ele2=float('inf')
+        for i in range(len(nums)):
+            if cnt1==0 and nums[i]!=ele2:
+                cnt1=1
+                ele1=nums[i]
+            elif cnt2==0 and nums[i]!=ele1:
+                cnt2=1
+                ele2=nums[i]
+            elif ele1==nums[i]:
+                cnt1+=1
+            elif ele2==nums[i]:
+                cnt2+=1
             else:
-                cnt1 -= 1
-                cnt2 -= 1
-
-        ls = [] # list of answers
-
-        # Manually check if the stored elements in
-        # el1 and el2 are the majority elements:
-        cnt1, cnt2 = 0, 0
-        for i in range(n):
-            if v[i] == el1:
-                cnt1 += 1
-            if v[i] == el2:
-                cnt2 += 1
-
-        mini = int(n / 3) + 1
-        if cnt1 >= mini:
-            ls.append(el1)
-        if cnt2 >= mini:
-            ls.append(el2)
-
-        # Uncomment the following line
-        # if it is told to sort the answer array:
-        #ls.sort() #TC --> O(2*log2) ~ O(1);
-
-        return ls
-
-
-
-            
-                
-       
-        
+                cnt1-=1
+                cnt2-=1
+        cnt1=0
+        cnt2=0
+        for i in range(len(nums)):
+            if nums[i]==ele1:
+                cnt1+=1
+            elif nums[i]==ele2:
+                cnt2+=1
+        ans=[]
+        mn=int(len(nums)/3)+1
+        if cnt1>=mn:
+            ans.append(ele1)
+        if cnt2>=mn:
+            ans.append(ele2)
+        return ans
+  
